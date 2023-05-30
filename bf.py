37944a21c98c2de54b3981280c9ed1681b3a6f2a
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-05-30 15:10:52 +0800

    .

--- bf.py
+++ bf.py
@@ -4,8 +4,25 @@ import os
 
 args = sys.argv[1:]
 argl = len(args)
+if not argl :
+    args = [ "" ]
 
-print(args)
+if argl == 1 and os.path.exists( args[0] ) :
+    file_path = args[0]
+    filetype = os.path.splitext( os.path.split( file_path )[-1] )[-1]
+    print(filetype)
+elif args[0] == "-b" and argl > 1 :
+    file_path = args[1]
+    print("b")
+elif args[0] == "-r" and argl > 1 :
+    file_path = args[1]
+    print("r")
+else : \
+print( \
+"""
+[ --build / -b ] [ --run / -r ] file
+run and build '.bfc' file | 运行并生成'.bfc'文件
 
-if argl == 1 :
-    print(args)
+-b : build '.bfc' file | 生成`.bfc`文件
+-r : run brainfuck file | 运行brainfuck文件
+""")
---
+++ bfc.py
@@ -0,0 +1,4 @@
+
+
+def run( file_path ) :
+    pass
