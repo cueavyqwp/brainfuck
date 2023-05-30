a2e62052468de8116d4e39bce839e8f1e3df4408
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-05-30 15:45:02 +0800

    .

--- bf.py
+++ bf.py
@@ -1,3 +1,10 @@
+"""
+主文件
+
+---
+
+main
+"""
 import bfc
 import sys
 import os
@@ -9,12 +16,21 @@ if not argl :
 
 if argl == 1 and os.path.exists( args[0] ) :
     file_path = args[0]
-    filetype = os.path.splitext( os.path.split( file_path )[-1] )[-1]
-    print(filetype)
-elif args[0] == "-b" and argl > 1 :
+    file_type = os.path.splitext( os.path.split( file_path )[-1] )[-1]
+    print(file_type)
+elif args[0] == "-b" and argl > 1 and os.path.exists( args[1] ) :
     file_path = args[1]
-    bfc.build( file_path )
-elif args[0] == "-r" and argl > 1 :
+    code_c =  bfc.build( file_path )
+    file_dir = os.path.split( file_path )[0]
+    if not file_dir :
+        file_dir = "." + os.sep
+    file_name = os.path.splitext( file_path )[0]
+    if argl > 3 and os.path.exists( args[3] ) :
+        code_c_path = os.path.join( args[3] , file_name + ".bfc" )
+    else :
+        code_c_path = os.path.join( file_dir , file_name + ".bfc" )
+    bfc.save_file( code_c_path , code_c )
+elif args[0] == "-r" and argl > 1 and os.path.exists( args[1] ) :
     file_path = args[1]
     print("r")
 else : \
@@ -23,6 +39,6 @@ print( \
 [ --build / -b ] [ --run / -r ] file
 run and build '.bfc' file | 运行并生成'.bfc'文件
 
--b : build '.bfc' file | 生成`.bfc`文件
+-b [ path ] : build '.bfc' file | 生成`.bfc`文件
 -r : run brainfuck file | 运行brainfuck文件
 """)
--- bfc.py
+++ bfc.py
@@ -1,12 +1,23 @@
+"""
+生成.bfc文件 和 运行brainfuck文件
+
+---
+
+build '.bfc' file and run brainfuck file
+"""
+
 def build( file_path ) :
     with open( file_path , "r" , encoding = "utf-8" ) as file :
         code = file.read()
-    print(code)
     code_c = ""
     for i in code :
         if i in [ ">" , "<" , "]" , "[" , "+" , "-" , "." , "," ] :
             code_c += i
     return code_c
+
+def save_file( code_c_path , code_c ) :
+    with open( code_c_path , "w" , encoding = "utf-8" ) as file :
+        file.write( code_c )
 
 def run( file_path ) :
     pass
