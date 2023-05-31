39399cd2cc1a9bc6fedec777c3039fa32af2c9e5
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-05-31 12:11:41 +0800

    .

--- wold.py
+++ wold.py
@@ -1,20 +1,18 @@
 while 1 :
-    try :
-        word = input(">")
-        for i in word :
-            I = ord(i)
-            num1 = I // 10
-            num2 = I % 10
-            if num2 >= 5 :
-                num1 += 1
-                num3 = 10 - num2
-                str3 = "-"
-            else :
-                num3 = num2
-                str3 = "+"
-            str1 = num1 * "+"
-            str2 = num3 * str3
-            ret = f">{str1}[<++++++++++>-]<{str2}.>"
-            print(ret)
-    except :
-        print()
+    num = int( input("Enter num\n>") )
+    word = input("Enter text\n>")
+    for i in word :
+        I = ord(i)
+        num1 = I // num
+        num2 = I % num
+        if num2 >= num//2 :
+            num1 += 1
+            num3 = num - num2
+            str3 = "-"
+        else :
+            num3 = num2
+            str3 = "+"
+        str1 = num1 * "+"
+        str2 = num3 * str3
+        ret = f">{str1}[<{'+'*num}>-]<{str2}.>"
+        print(ret)
