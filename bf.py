15c0eac2f03c24bfc1efb9fceae3adda555b60bb
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-07-31 11:03:04 +0800

    bug fix

--- bf.py
+++ bf.py
@@ -1,31 +1,61 @@
 """
-主文件
-
----
-
 main
 """
-import bfc
+
 import sys
-import os
 
-args = sys.argv[1:]
-argl = len(args)
-if not argl :
-    args = [ "" ]
+class bf :
 
-if argl == 1 and os.path.exists( args[0] ) :
-    file_path = args[0]
-    code = bfc.build( file_path )
-    bfc.run( code )
-elif args[0] == "-b" and argl > 1 and os.path.exists( args[1] ) :
-    file_path = args[1]
-    code = bfc.build( file_path )
-    bfc.save_file( file_path + "c" , code )
-else : \
-print( \
-"""
-[ --build / -b ] file
-run and build '.bfc' file | 运行并生成'.bfc'文件
--b [ path ] : build '.bfc' file | 生成`.bfc`文件
-""")
+    def __init__( self ) -> None :
+        self.code = ""
+
+    def build( self , path ) -> str :
+        with open( path , "r" , encoding = "utf-8" ) as file :
+            self.code = "".join( [ code if code in [ ">" , "<" , "]" , "[" , "+" , "-" , "." , "," ] else "" for code in file.read() ] )
+        return self.code
+
+    def save( self , path ) -> None :
+        with open( path , "w" , encoding = "utf-8" ) as file :
+            file.write( self.code )
+
+    def set( self , code ) -> None :
+        self.code = code
+
+    def run( self ) -> bool :
+        code = self.code
+        memory = [ 0 ]
+        code_p = 0
+        p = 0
+        while code_p < len( code ) :
+            match code[ code_p ] :
+                case ">" :
+                    p += 1
+                    if len( memory ) <= p :
+                        memory.append( 0 )
+                case "<" :
+                    p -= 1
+                case "+" :
+                    memory[ p ] += 1
+                case "-" :
+                    memory[ p ] -= 1
+                case "," :
+                    while True :
+                        try :
+                            memory[ p ] = ord( input() )
+                            break
+                        except TypeError :
+                            pass
+                case "." :
+                    print( chr( memory[ p ] ) , end = "" )
+                case "[" :
+                    if not memory[ p ] :
+                        code_p += code[ code_p : ].index( "]" )
+                case "]" :
+                    code_p = code[ : code_p ].rindex( "[" ) - 1
+            code_p += 1
+
+if __name__ == "__main__" :
+    args = sys.argv[ 1 : ]
+    main = bf()
+    main.build( args[ -1 ] )
+    main.run()
